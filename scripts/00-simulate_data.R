a4c91d8871507dd8a9e5983baf379ce266984a43
Jeongwoo Kim <Kjeongwoo@gmail.com>
2024-01-23 15:41:08 +0000

    first commit

--- scripts/00-simulate_data.R
+++ scripts/00-simulate_data.R
@@ -1,11 +1,9 @@
 #### Preamble ####
-# Purpose: Simulates... [...UPDATE THIS...]
-# Author: Rohan Alexander [...UPDATE THIS...]
-# Date: 11 February 2023 [...UPDATE THIS...]
-# Contact: rohan.alexander@utoronto.ca [...UPDATE THIS...]
+# Purpose: Simulates the GHG emissions by country data 
+# Author: Jeongwoo Kim 
+# Date: 23 January 2024
+# Contact: jwoo.kim@mail.utoronto.ca
 # License: MIT
-# Pre-requisites: [...UPDATE THIS...]
-# Any other information needed? [...UPDATE THIS...]
 
 
 #### Workspace setup ####
@@ -13,7 +11,20 @@ library(tidyverse)
 # [...UPDATE THIS...]
 
 #### Simulate data ####
-# [...ADD CODE HERE...]
+set.seed(100)
+
+countries <- c('China', 'United States', 'India', 'Russia', 'Indonesia',
+               'Brazil', 'Japan', 'Iran', 'Canada', 'Saudi Arabia')
+
+simulated_data <- 
+  tibble (
+    # select 10 most GHG emitting countries 
+    "country" = countries,
+    # make it to the most recent year: 2020
+    "year" = rep(2000, length(countries)),
+    # randomly assign each country with GHG emissions ranging from 0 to the maximum of 13000 
+    "emissions" = runif(length(countries), min = 0, max = 13000)
+  )
 
 
 
